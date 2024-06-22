3edc36d4a5e98234a81708f26355a932ce5677d1
tomerbabila <tomer.babila@gmail.com>
2024-06-22 23:17:46 +0300

    Create i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
+import './i18n/i18n';
+import { useTranslation } from 'react-i18next';
 
 function App() {
-  return <div className='App'>Hello World!</div>;
+  const { t } = useTranslation();
+
+  return <div className='App'>{t('general.name')}</div>;
 }
 
 export default App;
